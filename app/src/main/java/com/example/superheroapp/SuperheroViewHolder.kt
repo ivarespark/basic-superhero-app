79641ffd4d248210c54ea8a3e389a55f7dd3601c
Ivan Espinosa <ivarespar@gmail.com>
2023-10-25 22:47:38 -0500

    [o24] Mostrar imagen superheroe en lista

--- app/src/main/java/com/example/superheroapp/SuperheroViewHolder.kt
+++ app/src/main/java/com/example/superheroapp/SuperheroViewHolder.kt
@@ -3,6 +3,7 @@ package com.example.superheroapp
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.superheroapp.databinding.ItemSuperheroBinding
+import com.squareup.picasso.Picasso
 
 class SuperheroViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
@@ -10,5 +11,6 @@ class SuperheroViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     fun bind(superheroItem: SuperheroItem){
         binding.tvSuperheroName.text = superheroItem.name // amarro nombre con textView
+        Picasso.get().load(superheroItem.image.url).into(binding.ivSuperhero) // carga imagen en imageview
     }
 }
